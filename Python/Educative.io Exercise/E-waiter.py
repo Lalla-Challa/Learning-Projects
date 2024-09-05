e3e86c7bd5f0feeb6736c1e3d09ec1a56e4e16d6
unknown <iamabdullah09@gmail.com>
2024-09-05 12:13:48 +0500

    learning

---
+++ Python/Educative.io Exercise/E-waiter.py
@@ -0,0 +1,32 @@
+print("Welcome to Our Restaurant!")
+name = input("May I have your name, please? ")
+
+print("Hello", name, "! Here is our menu:")
+print("In starters, we have some suggestions: ")
+print("Soup, Salad, Garlic Bread")
+print() #new line
+
+order_starters = input("What would you like for starter? ")
+
+# Add code for the main menu here. Main menu item suggestions include: Steak, Pasta, Burger
+
+print("In Main Menu, we have some suggestions")
+print("Steak, Pasta, Burger") 
+print() #new line
+
+order_Main = input ("What would you like for Main Menu? ")
+# Add code for dessert here. Dessert item suggestions include: Cake, Ice Cream, Fruit Salad
+
+print("In Dessert, we have some suggestions")
+print("Cake, Ice Cream, Fruit Salad")
+print() #new line
+
+order_Desserts = input("What would you like for Desserts? ")
+print() #new line
+
+print("Thank You", name, "I am repating your order")
+print() #new line
+
+print(order_starters, "for starters")
+print(order_Main, "for Main Menu")
+print(order_Desserts, "for Desserts")
