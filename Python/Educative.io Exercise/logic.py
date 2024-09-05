16c5fa3077c82c3aa4dfcd10aac6ec344b2c764b
Lalla-Challa <iamabdullah09@gmail.com>
2024-09-05 17:57:02 +0500

    Learning

---
+++ Python/Educative.io Exercise/logic.py
@@ -0,0 +1,19 @@
+#Code to swap the positions of two variables
+
+print("Please Enter num 1?")
+num_1=int=input()
+
+print() # Next line
+
+print("Please Enter num 2?")
+num_2=int=input()
+
+#Swapping positions
+
+num_3=num_1
+num_1=num_2
+num_2=num_3
+
+print() # Next line
+
+print(num_1, num_2) 
